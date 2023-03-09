05120520fd3c802e89197421598d69df8317fedd
Dmitri Mandaji <badmopss@gmail.com>
2023-03-09 19:00:16 +0100

    addPrivate int

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,13 +1,17 @@
 public class Triangle {
-  private int side1;
-  private int side2;
-  private int side3;
+  private int sideA;
+  private int sideB;
+  private int sideC;
 
   public Triangle(int a, int b, int c) {
     if (a <= 0 || b <= 0 || c <= 0) {
       System.out.println("Ошибка: длины сторон треугольника не могут быть отрицательными или равными нулю.");
     } else if (!checkSides(a, b, c)) {
       System.out.println("Ошибка: невозможно построить треугольник с заданными сторонами.");
+    } else {
+      sideA = a;
+      sideB = b;
+      sideC = c;
     }
   }
 
