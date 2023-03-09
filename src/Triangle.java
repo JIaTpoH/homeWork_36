ebf248c37b63126451131e9a723b46490c017fcd
Dmitri Mandaji <badmopss@gmail.com>
2023-03-09 17:08:55 +0100

    TriangleError

--- src/Triangle.java
+++ src/Triangle.java
@@ -3,5 +3,11 @@ public class Triangle {
   private int side2;
   private int side3;
 
-
+  public Triangle(int a, int b, int c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      System.out.println("Ошибка: длины сторон треугольника не могут быть отрицательными или равными нулю.");
+    } else if (!checkSides(a, b, c)) {
+      System.out.println("Ошибка: невозможно построить треугольник с заданными сторонами.");
+    }
+  }
 }
