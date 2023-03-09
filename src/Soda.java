ea35f566248565a4c64fdeb1b546a57abc17b707
Dmitri Mandaji <badmopss@gmail.com>
2023-03-09 16:55:09 +0100

    GetMyDrinkString

--- src/Soda.java
+++ src/Soda.java
@@ -4,5 +4,12 @@ public class Soda {
   public Soda(String addOn) {
     this.addOn = addOn;
   }
-}
 
+  public String GetMyDrinkString() {
+    if (addOn == null || addOn.isEmpty()) {
+      return "Обычная газировка";
+    } else {
+      return "Газировка и " + addOn;
+    }
+  }
+}
